7b9efc458bef5ddd980ac6abfce04d396a2d6b5c
Jorge79 <Jorge7.9@hotmail.com>
2019-05-22 16:24:07 -0300

    No comparison

--- js/app.js
+++ js/app.js
@@ -1,10 +1,19 @@
+var moves = 0;
+var allCards;
+var movements;
+var minutesLabel;
+var secondsLabel;
+var totalSeconds;
+var matchedCards;
+var interval;
+
 var cards = ['fa-diamond', 'fa-diamond', 'fa-paper-plane-o', 'fa-paper-plane-o',
             'fa-anchor', 'fa-anchor', 'fa-bolt', 'fa-bolt', 'fa-cube', 'fa-cube',
-            'fa-leaf', 'fa-leaf', 'fa-bycicle', 'fa-bycicle', 'fa-bomb', 'fa-bomb'
+            'fa-leaf', 'fa-leaf', 'fa-bicycle', 'fa-bicycle', 'fa-bomb', 'fa-bomb'
             ];
 
-function generateCard(cards) {
-    return `<li class="card"><i class=fa ${cards}"></i></li>`;
+function generateCard(card) {
+    return `<li class="card"><i class="fa ${card}"> </i> </li>`;
 }
 
 // Shuffle function from http://stackoverflow.com/a/2450976
@@ -25,22 +34,29 @@ function initGame() {
     var deck = document.querySelector('.deck');
 
     var cardHTML = shuffle(cards).map(function(card) {
-        return generateCard();
+        return generateCard(card);
     });
-    moves = 0;
+
     deck.innerHTML = cardHTML.join('');
+    allCards = document.querySelectorAll('.card');
+    restart();
+    openCards = [];
+    movements = 0;
+    moves = document.getElementById("moves");
+    moves.innerHTML= movements;
+
+    minutesLabel = document.getElementById("minutes");
+    secondsLabel = document.getElementById("seconds");
+    totalSeconds = 0;
+    document.getElementById("star1").className = "fa fa-star";
+    document.getElementById("star2").className = "fa fa-star";
+    document.getElementById("star3").className = "fa fa-star";
+
+    matchCards =0;
+    first = true;
 }
 
 initGame();
-
-var allCards = document.querySelectorAll('.card');
-var openCards = [];
-var moves = document.getElementById("moves");
-
-var minutesLabel = document.getElementById("minutes");
-var secondsLabel = document.getElementById("seconds");
-var totalSeconds = 0;
-setInterval(setTime, 1000);
 
 function setTime() {
     ++totalSeconds;
@@ -57,15 +73,18 @@ function pad(val) {
     }
 }
 
-function game() {
+function restart() {
     allCards.forEach(function(card) {
         card.addEventListener('click', function() {
+            if (openCards[0] == openCards[1]) {
+                interval = setInterval(setTime, 1000);
+            }
             if (!card.classList.contains('open') && !card.classList.contains('show') && !card.classList.contains('match')) {
                 openCards.push(card);
                 card.classList.add('open', 'show');
 
-                if (openCards.length == 2) {
-                    if (openCards[0].dataset.card == openCards[1].dataset.card) {
+                if (openCards.length == 2) { //Two cards open
+                    if (openCards[0].dataset.card == openCards[1].dataset.card) { //Cards equal
                         openCards[0].classList.add('match');
                         openCards[0].classList.add('open');
                         openCards[0].classList.add('show');
@@ -75,7 +94,16 @@ function game() {
                         openCards[1].classList.add('show');
 
                         openCards = [];
-                    } else {
+                        matchedCards++;
+                        if (matchedCards == 8) {
+                            setTimeout(() => {
+                                alert("Congratulations!!");
+                                clearInterval(interval);
+                                initGame()
+                            },1000);
+                        }
+                    }
+                    else {
                         setTimeout(function() {
                             openCards.forEach(function(card) {
                                 card.classList.remove('open', 'show');
@@ -83,8 +111,25 @@ function game() {
                         openCards = [];
                         }, 1000);
                     }
-                    moves += 1;
-                    moves.innerHTML= moves;
+                    movements++;
+                    moves.innerHTML=movements;
+                    if(movements <= 10) {
+                        document.getElementById("star1").className ="fa fa-star";
+                        document.getElementById("star2").className ="fa fa-star";
+                        document.getElementById("star3").className ="fa fa-star";
+                    } else if(movements <= 14){
+                        document.getElementById("star1").className ="fa fa-star";
+                        document.getElementById("star1").className ="fa fa-star";
+                        document.getElementById("star1").className ="far fa-star";
+                    } else if(movements <= 18){
+                        document.getElementById("star1").className ="fa fa-star";
+                        document.getElementById("star1").className ="far fa-star";
+                        document.getElementById("star1").className ="far fa-star";
+                    }else{
+                        document.getElementById("star1").className ="fa fa-star";
+                        document.getElementById("star1").className ="fa fa-star";
+                        document.getElementById("star1").className ="far fa-star";
+                    }
                 }
             }
         });
@@ -99,40 +144,7 @@ function game() {
  *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
-
- /*function greeting(name) {
+ *function greeting(name) {
   alert('Hello ' + name);
 }
-
-function greeting2(name2) {
-    alert('Hello ' + name2 + 'How are you?');
-}
-
-function processUserInput(cbGreeting) {
-  var name2 = prompt('Please enter your name.');
-  cbGreeting(name2);
-}
-Higher Order Functions
-
-processUserInput(greeting2);
-
-function t(val1, cb) {
-    if (val1 === 'authenticated') {
-        cb();
-    }
-    else{
-        console.log('Value is false');
-    }
-}
-
-function sum(callback, n2) {
-    const n1 = callback();
-    return n1 + n2;
-}
-
-function number() {
-    return 1;
-}
-
-console.log(sum(number, 3));*/
+*/
